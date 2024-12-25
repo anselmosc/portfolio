2ab358adcd95a9072bd31860f630ee45c7548c6b
anselmosc <anselmosantana@gmail.com>
2024-12-25 20:30:52 -0300

    atualização portfolio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-function toggleMode() {
-    const html = document.documentElement;
-    html.classList.toggle("light");
+function toggleMode() { //Função que alterna entre os modos claro e escuro
+    const html = document.documentElement; //Seleciona o elemento HTML (<html>) da página
+    html.classList.toggle("light"); //Alterna a classe light no elemento HTML. Se a classe existir, ela é removida; se não, ela é adicionada
 
     //Alterar imagem do perfil
-    const img = document.querySelector("#profile img");
+    const img = document.querySelector("#profile img"); //Seleciona a imagem dentro do elemento com o id profile (a foto do perfil)
 
     //Condicional para verificar se a classe "light" foi adicionada ao elemento HTML
-    if (html.classList.contains("light")) {
-        img.setAttribute("src", "./img/imagem linkedin.jfif");
+    if (html.classList.contains("light")) { //Verifica se o elemento HTMLcontém a classe light (modo claro)
+        img.setAttribute("src", "./img/imagem linkedin.jfif"); //Se estiver no modo claro, altera a imagem
     } else {
-        img.setAttribute("src", "./img/Anselmo_social.jpeg");
+        img.setAttribute("src", "./img/Anselmo_social.jpeg"); //Se não estiver no modo claro (modo escuro), mantém ou coloca a imagem
     }        
 
     }
